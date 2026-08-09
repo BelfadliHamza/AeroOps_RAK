74dfea29f08a268e6abbf8839b0f24ea2fcec842
BelfadliHamza <hamzabelfadli99@gmail.com>
2026-08-09 14:07:04 +0200

    chore: init projet - config Spring Boot + Docker

---
+++ backend/src/main/java/com/onda/aeroops/AeroOpsRakApplication.java
@@ -0,0 +1,11 @@
+package com.onda.aeroops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroOpsRakApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AeroOpsRakApplication.class, args);
+    }
+}
